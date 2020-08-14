36ddadc93832d34d36629896a49fe9ec373690e9
Palak <palakgupta2712@gmail.com>
2020-08-15 03:08:03 +0530

    Reverse of an array

---
+++ Data structures/Array/reverse_array.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class reverse_array{
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        Scanner s = new Scanner(System.in);
+        int i = 0;
+        //Input Elements into the array.
+        System.out.println("ENTER VALUES IN THE ARRAY:");
+        for (i=0; i<5; i++){
+            arr[i] = s.nextInt();
+        }
+        
+        System.out.println("The original of the array is:");
+        for(i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + "  ");
+        }
+
+        int temp;
+        int start = 0;
+        int end = arr.length-1;
+        //loop through all the elements and swap the start and end elements.
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }    
+
+        System.out.println("\n The reverse of the array is:");
+        for(i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + "  ");
+        }
+    }
+}
